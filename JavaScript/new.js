5d1156e5fa4cdd16c73533a68e24582477cc9b72
ShinKhantXoX <shinkhantlaybal@gmail.com>
2022-03-19 11:44:30 +0630

    callback function

---
+++ JavaScript/new.js
@@ -0,0 +1,266 @@
+let data = {
+    "status": "ok",
+    "totalResults": 9574,
+    "articles": [
+    {
+    "source": {
+    "id": "business-insider",
+    "name": "Business Insider"
+    },
+    "author": "insider@insider.com (Cheryl Teh)",
+    "title": "Elon Musk says Vladimir Putin can 'bring his bear' to their man-on-man fight over Ukraine",
+    "description": "Musk this week challenged the Russian leader to \"single combat,\" with the stakes being \"Ukraine,\" even tagging the Kremlin in his initial tweet.",
+    "url": "https://www.businessinsider.com/elon-musk-vladimir-putin-can-bring-bear-to-fight-2022-3",
+    "urlToImage": "https://i.insider.com/623147c6843ef30018e73ceb?width=1200&format=jpeg",
+    "publishedAt": "2022-03-16T03:26:09Z",
+    "content": "Tesla and SpaceX CEO Elon Musk is upping the ante on his proposed showdown with Russian leader Vladimir Putin over Ukraine, telling the Russian president he could \"bring his bear\" to the fight betwee… [+2172 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "New Zealand Herald"
+    },
+    "author": "newsfeeds@nzherald.co.nz",
+    "title": "Elon Musk changes Twitter name after taunts from Chechen leader Ramzan Kadyrov",
+    "description": "In a bizarre move, Elon Musk has changed his name on Twitter from Elon to Elona, after his bid to fight Russian President Vladamir Putin received attention from infamous dictator and Russian ally, Ramzan Kadyrov.For background,...",
+    "url": "https://www.nzherald.co.nz/technology/elon-musk-changes-twitter-name-after-taunts-from-chechen-leader-ramzan-kadyrov/OWHRVOK7LVRXTYI7YDMPWVB6KQ/",
+    "urlToImage": "https://www.nzherald.co.nz/resizer/AIq6Y7DW3Hh3FPN3NA8xUhk7DW4=/1200x675/filters:quality(70)/cloudfront-ap-southeast-2.images.arcpublishing.com/nzme/TEUWJ6A6S6E4S2OTXET7VEZKAU.jpg",
+    "publishedAt": "2022-03-16T03:24:01Z",
+    "content": "Elon Musk. Photo / Getty ImagesIn a bizarre move, Elon Musk has changed his name on Twitter from Elon to Elona, after his bid to fight Russian President Vladamir Putin received attention from infamou… [+4479 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Viva.co.id"
+    },
+    "author": "lintang ayu",
+    "title": "Kemewahan Sultan Abal-abal",
+    "description": "Berita Kemewahan Sultan Abal-abal terbaru hari ini 2022-03-16 10:07:02 dari sumber yang terpercaya",
+    "url": "https://www.viva.co.id/vstory/opini-vstory/1457952-kemewahan-sultan-abal-abal",
+    "urlToImage": "https://thumb.viva.co.id/media/frontend/thumbs3/2022/03/16/623154449cfdc-kemewahan-sultan-abal-abal_665_374.jpg",
+    "publishedAt": "2022-03-16T03:07:02Z",
+    "content": "VIVA  Nama Indra Kenz atau Indra Kesuma menjadi sorotan netizen Indonesia sejak muncul di TikTok dengan gayanya selalu menyombongkan kekayaan. Awal kemunculannya menjadi buat bibir netizen Indonesia … [+1771 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Chosun.com"
+    },
+    "author": "By Kim A-sa ( )",
+    "title": "Are Core Customers Bored of Mercedes",
+    "description": "Mercedes-Benz's dominant position in the Korean import-car market is under threat as its main customer base aged 20 to 49 is turning to other brands.According to the Korea Automobile Importers and Distributors Association, Mercedes-Benz sold 76,152 cars last …",
+    "url": "https://english.chosun.com/site/data/html_dir/2022/03/16/2022031601294.html",
+    "urlToImage": "https://english.chosun.com/site/data/img_dir/2022/03/16/2022031601286_0.jpg",
+    "publishedAt": "2022-03-16T02:34:20Z",
+    "content": "Mercedes-Benz's dominant position in the Korean import-car market is under threat as its main customer base aged 20 to 49 is turning to other brands.\r\nAccording to the Korea Automobile Importers and … [+953 chars]"
+    },
+    {
+    "source": {
+    "id": "la-nacion",
+    "name": "La Nacion"
+    },
+    "author": null,
+    "title": "Elon Musk inscribió su empresa de internet en la Argentina",
+    "description": "El dueño de Tesla y Space X inscribió Starlink en el país, según se publicó en el Boletín Oficial",
+    "url": "https://www.lanacion.com.ar/politica/elon-musk-inscribio-su-empresa-de-internet-en-la-argentina-nid15032022/",
+    "urlToImage": "https://resizer.glanacion.com/resizer/K4TdDwV1IxyDc5NkOKAVqdLrSHc=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CMFYHSRWANC2HLOOWQPGVPQ6WY",
+    "publishedAt": "2022-03-16T02:25:53Z",
+    "content": "Starlink, la empresa de Elon Musk que ofrece servicio de internet, se registró en el país como sociedad en la Inspección General de Justicia. Según se publicó en el Boletín Oficial de este martes, la… [+1619 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yandex.ru"
+    },
+    "author": "RT на русском",
+    "title": "Кадыров заявил, что уважает Маска за собственное видение ситуации и непоколебимую волю",
+    "description": "«Глава Чечни Рамзан Кадыров заявил, что уважает основателя SpaceX и Tesla Илона Маска как человека «со своим видением ситуации и непоколебимой волей».» — RT на русском\n«Позже Маск сообщил, что спутниковая связь Starlink заработала на Украине.» — Телеканал 360°",
+    "url": "https://yandex.ru/news/story/Kadyrov_zayavil_chto_uvazhaet_Maska_zasobstvennoe_videnie_situacii_i_nepokolebimuyu_volyu--300a9c9411cc1205c4fddf9221ba1a4d",
+    "urlToImage": "https://avatars.mds.yandex.net/get-ynews/2815965/fe15e75921c8d45cd5968f88d371ccca/496x248",
+    "publishedAt": "2022-03-16T02:24:12Z",
+    "content": ""
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "FX Empire"
+    },
+    "author": "Reuters",
+    "title": "Chinese EV maker BYD raises prices citing jump in raw materials costs",
+    "description": "SHANGHAI (Reuters) -   Chinese electric vehicle (EV) maker BYD Co Ltd late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost of raw materials.",
+    "url": "https://www.fxempire.com/news/article/chinese-ev-maker-byd-raises-prices-citing-jump-in-raw-materials-costs-936253",
+    "urlToImage": "https://responsive.fxempire.com/width/600/webp-lossy-70.q50/_fxempire_/2022/03/tagreuters.com2022newsml_LYNXNPEI2F0221.jpg",
+    "publishedAt": "2022-03-16T01:48:20Z",
+    "content": "Due to the impact of continued rises in raw material prices, BYD has officially guided Wang Chao Business and Ocean Network Business to adjust prices on relevant EV models, it said in a statement on … [+848 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Techweb.com.cn"
+    },
+    "author": "Techweb",
+    "title": "特斯拉员工被解雇：因发布自动驾驶系统负面测评视频",
+    "description": "北京时间3月16日早间消息，据报道，日前，特斯拉解雇了一名曾经在自动驾驶部门（Autopilot）工作过的员工约翰·博纳（John Berna），原因是此人在Youtube个人频道“AI发烧友”上公布了对于特斯拉全自动驾驶系统（FSD）的评论。他的视频展现了这一系统的测试版在硅谷不同路况上如何工作的场景，以及存在的问题。\n在博纳被解雇之后，特斯拉还禁止他的私人车辆——一辆2021款全自动驾驶系统测试版Model 3。\n据报道，测试版的全自动驾驶系统相当于诸多辅助驾驶功能的集合，这些功能的开发尚未结束，仍然可能存在…",
+    "url": "http://www.techweb.com.cn/smarttraveling/2022-03-16/2883594.shtml",
+    "urlToImage": "http://upload1.techweb.com.cn/s/120/2022/0316/1647395143121.png",
+    "publishedAt": "2022-03-16T01:45:45Z",
+    "content": "316Autopilot·John BernaYoutube“AI”FSD\r\n——2021Model 3\r\n“”\r\n1.2199\r\n202082\r\n2021Model 320201226\r\n8000\r\n20212Youtubue“AI”\r\nFSD\r\nFSD“Dirty Tesla”Chuck CookKim Paquette\r\nFSD\r\nLinkedinYoutube\r\nFacebookInst… [+126 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "WRAL.com"
+    },
+    "author": "Joe Fisher",
+    "title": "Electric cars are more pricey than gas-powered ones. Do they actually save you cash in the long run? :: WRAL.com",
+    "description": "Electric cars are more pricey than gas-powered ones. Do they actually save you cash in the long run? :: WRAL.comwral.com",
+    "url": "https://www.wral.com/electric-cars-are-more-pricey-than-gas-powered-ones-do-they-actually-save-you-cash-in-the-long-run/20188497/",
+    "urlToImage": "https://wwwcache.wral.com/asset/news/local/2021/11/08/19969906/screenshot-DMID1-5u7xjxg6d-640x480.jpg",
+    "publishedAt": "2022-03-16T01:35:39Z",
+    "content": "By Joe Fisher, WRAL reporter\r\nAs interest in electric cars climbs along with gas prices, some incentives have expired\r\nInterest is rising in electric vehicles with the average price of gas in North C… [+1521 chars]"
+    },
+    {
+    "source": {
+    "id": "rt",
+    "name": "RT"
+    },
+    "author": "RT en Español\n , RT en Español",
+    "title": "Musk reta a Putin en Twitter y se cambia de nombre tras recibir un comentario del líder de Chechenia",
+    "description": "El intercambio de mensajes en redes se desencadenó después de que este lunes el multimillonario desafiara al presidente ruso a un combate cuerpo a cuerpo con Ucrania como premio.",
+    "url": "https://actualidad.rt.com/actualidad/423997-musk-cambia-nombre-twitter-consejo-kadyrov",
+    "urlToImage": "https://cdni.rt.com/actualidad/public_images/2022.03/article/623123a7e9ff716b5445a6fe.jpg",
+    "publishedAt": "2022-03-16T01:26:53Z",
+    "content": "El fundador de Tesla y SpaceX, Elon Musk, cambió de nombre en su cuenta de Twitter, a raíz de una publicación en Telegram del líder de la república rusa de Chechenia, Ramzán Kadyrov en la que le llam… [+2850 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Reuters",
+    "title": "Chinese EV maker BYD raises prices citing jump in raw materials costs",
+    "description": "Chinese electric vehicle (EV) maker BYD Co Ltd late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost ...",
+    "url": "https://finance.yahoo.com/news/chinese-ev-maker-byd-raises-012429986.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/yUk7fDjNjvVEfmGjE9HxLA--~B/aD01MzM7dz04MDA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/reuters.com/54efd143f9522a975e57ac3f2baa64f1",
+    "publishedAt": "2022-03-16T01:24:29Z",
+    "content": "SHANGHAI (Reuters) - Chinese electric vehicle (EV) maker BYD Co Ltd late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost of raw materials.\r\nTh… [+1204 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CNA"
+    },
+    "author": null,
+    "title": "Chinese EV maker BYD raises prices citing jump in raw materials costs",
+    "description": "SHANGHAI :     Chinese electric vehicle (EV) maker BYD Co Ltd late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost of raw materials.The price increases will take effect from Wednesday, it said, adding t…",
+    "url": "https://www.channelnewsasia.com/business/chinese-ev-maker-byd-raises-prices-citing-jump-raw-materials-costs-2567491",
+    "urlToImage": "https://onecms-res.cloudinary.com/image/upload/s--tC0aiDRv--/fl_relative,g_south_east,l_one-cms:core:watermark:reuters,w_0.1/f_auto,q_auto/c_fill,g_auto,h_676,w_1200/v1/one-cms/core/2022-03-16t012429z_1_lynxnpei2f022_rtroptp_3_autoshow-shanghai-byd.jpg?itok=wCzMRXmW",
+    "publishedAt": "2022-03-16T01:24:29Z",
+    "content": "SHANGHAI : Chinese electric vehicle (EV) maker BYD Co Ltd late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost of raw materials.\r\nThe price in… [+1196 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CNA"
+    },
+    "author": null,
+    "title": "Chinese EV maker BYD raises prices citing jump in raw materials costs",
+    "description": "SHANGHAI: Chinese electric vehicle (EV) maker BYD late on Tuesday (Mar 15) said it would raise prices on its cars by 3,000 to 6,000 yuan (US$471 to US$942), citing the rising cost of raw materials.\r\n\r\nThe price increases will take effect from Wednesday, it sa…",
+    "url": "https://www.channelnewsasia.com/business/china-electric-vehicle-maker-byd-raise-prices-cars-raw-materials-costs-2567491",
+    "urlToImage": "https://onecms-res.cloudinary.com/image/upload/s--Ve3zm5F5--/fl_relative,g_south_east,l_one-cms:core:watermark:reuters,w_0.1/f_auto,q_auto/c_fill,g_auto,h_676,w_1200/v1/one-cms/core/2022-03-16t014820z_2_lynxnpei2f022_rtroptp_3_autoshow-shanghai-byd.jpg?itok=OK51wJdR",
+    "publishedAt": "2022-03-16T01:24:00Z",
+    "content": "SHANGHAI: Chinese electric vehicle (EV) maker BYD late on Tuesday (Mar 15) said it would raise prices on its cars by 3,000 to 6,000 yuan (US$471 to US$942), citing the rising cost of raw materials.\r\n… [+1070 chars]"
+    },
+    {
+    "source": {
+    "id": "reuters",
+    "name": "Reuters"
+    },
+    "author": null,
+    "title": "Chinese EV maker BYD raises prices citing jump in raw materials costs - Reuters.com",
+    "description": "Chinese electric vehicle (EV) maker BYD Co Ltd <a href=\"https://www.reuters.com/companies/002594.SZ\" target=\"_blank\">(002594.SZ)</a> late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost of raw materials.",
+    "url": "https://www.reuters.com/business/autos-transportation/chinese-ev-maker-byd-raises-prices-citing-jump-raw-materials-costs-2022-03-16/",
+    "urlToImage": "https://www.reuters.com/resizer/nV5tBduQwyfijYRrxAxnLzhkEZo=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/DO7SWXBDQBP37IEWN7NTPQIOGE.jpg",
+    "publishedAt": "2022-03-16T01:24:00Z",
+    "content": "SHANGHAI, March 16 (Reuters) - Chinese electric vehicle (EV) maker BYD Co Ltd (002594.SZ) late on Tuesday said it would raise prices on its cars by 3,000-6,000 yuan ($471-942), citing the rising cost… [+1408 chars]"
+    },
+    {
+    "source": {
+    "id": "business-insider",
+    "name": "Business Insider"
+    },
+    "author": "tmitchell@insider.com (Taiyler Simone Mitchell)",
+    "title": "Elon Musk doubles down on his challenge to fight Putin: 'If he is afraid to fight, I will agree to use only my left hand and I am not even left-handed'",
+    "description": "In Elon Musk's original tweet he challenged Putin to one-on-one combat over Ukraine, which Russia invaded on February 24.",
+    "url": "https://www.businessinsider.com/elon-musk-doubles-down-challenge-fight-putin-2022-3",
+    "urlToImage": "https://i.insider.com/62308149dc43bd0018946df6?width=1200&format=jpeg",
+    "publishedAt": "2022-03-16T01:23:47Z",
+    "content": "Tesla and SpaceX CEO Elon Musk doubled down on his challenge to fight Russian President Vladimir Putin saying he'd fight the Russian dictator with his left hand.\r\nMusk tweeted in response to an appar… [+2095 chars]"
+    },
+    {
+    "source": {
+    "id": "techcrunch",
+    "name": "TechCrunch"
+    },
+    "author": "Haje Jan Kamps,Nariko Mizoguchi",
+    "title": "電力会社のメーターをスマート化するCopper Labsが6.5億円調達",
+    "description": "電力会社は問題を抱えている。電力会社の「スマートグリッド」は10年前の請求問題を解決するために作られたもので、電気自動車、ソーラーパネル、そしてリアルタイムデータへのこだわりを持つ2022年の消費者のニーズや期待に応えるためのものではないからだ。この問題を解決すべく、処理能力の低いスマートメーターと消費者のインターネット接続の間を橋渡しする、エレガントで小さなハードウェアデバイスを展開する<a target=\"_blank\" href=\"https://www.copperlabs.com/\" rel=\"noop…",
+    "url": "https://jp.techcrunch.com/2022/03/16/2022-03-15-copper-labs-series-a/",
+    "urlToImage": "https://jp.techcrunch.com/wp-content/uploads/2022/03/Copper-.webp?w=1024",
+    "publishedAt": "2022-03-16T01:15:19Z",
+    "content": "102022Copper Labs55065000\r\nSmart Grid15Copper LabsCEODan Forman301301\r\nCopper LabsClean Energy Ventures CEV550National Grid PartnersBlue Bear CapitalCopper Labs2023Clean Energy VenturesNora Mead Brow… [+249 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Haber7.com"
+    },
+    "author": "Haber7.com",
+    "title": "Çeçen lideri Kadirov'dan Elon Musk'a tehditvari mesaj!",
+    "description": "Tesla ve SpaceX CEO'su Elon Musk, geçtiğimiz günlerde Rusya Devlet Başkanı Vladimir Putin’e sosyal medya hesabından meydan okumuş ve \"teke tek dövüş\" teklifinde bulunmuştu. Elon Musk'un bu teklifine Çeçenlerden yanıt gecikmedi.",
+    "url": "https://www.haber7.com/dunya/haber/3202929-cecen-lideri-kadirovdan-elon-muska-tehditvari-mesaj",
+    "urlToImage": "https://i20.haber7.net/resize/1240x720//haber/haber7/photos/2022/11/cecen_lideri_kadirovdan_elon_muska_tehditvari_mesaj_1647393028_5755.jpg",
+    "publishedAt": "2022-03-16T01:10:00Z",
+    "content": "Tesla ve SpaceX CEO'su Elon Musk, Ukrayna'ya saldrlarn sürdüren Rusya Devlet Bakan Vladimir Putin'e meydan okumu ve teke tek dövüe davet etmiti. Musk'n bu mesajna Çeçenistan Cumhurbakan Ramazan Kadir… [+2159 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Freerepublic.com"
+    },
+    "author": "Wall Street Journal",
+    "title": "In Congo, China Hits Roadblock in Global Race for Cobalt (Hunter helped to make the deal)",
+    "description": "For more than a decade, Chinese companies have spent billions of dollars buying out U.S. and European miners in the Democratic Republic of Congo’s cobalt belt - the world’s richest source of a mineral that has become critical to the global transition to clean…",
+    "url": "https://freerepublic.com/focus/f-chat/4047072/posts",
+    "urlToImage": null,
+    "publishedAt": "2022-03-16T00:51:34Z",
+    "content": "Skip to comments.\r\nIn Congo, China Hits Roadblock in Global Race for Cobalt (Hunter helped to make the deal)Wall Street Journal ^\r\n | 3/12/22\r\n | Nicholas Bariyo\r\nPosted on 03/15/2022 5:51:34 PM PDT … [+3263 chars]"
+    },
+    {
+    "source": {
+    "id": "abc-news-au",
+    "name": "ABC News (AU)"
+    },
+    "author": "Kym Agius",
+    "title": "Queenslanders will be offered cash to switch over to an electric car. Here's what you need to know",
+    "description": "A subsidy for new electric vehicles has been announced to persuade Queensland motorists to switch over, with the sector growing exponentially. This is how it will work.",
+    "url": "https://www.abc.net.au/news/2022-03-16/qld-subsidy-electric-car-vehicle-new-3000-charging-station/100913100",
+    "urlToImage": "https://live-production.wcms.abc-cdn.net.au/5d2bceeac7a30556db7b470e06217c82?impolicy=wcms_crop_resize&cropH=539&cropW=957&xPos=1&yPos=0&width=862&height=485",
+    "publishedAt": "2022-03-16T00:46:08Z",
+    "content": "Queenslanders are being offered a $3,000 subsidy from the state government to buy an electric car.\r\nWith unleaded petrol prices topping $2.20 a litre, it's hoped the cash will motivate some Queenslan… [+3516 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Observador.pt"
+    },
+    "author": "José Carlos Duarte",
+    "title": "Antes do duelo com Putin, líder checheno aconselha Musk a treinar. Dono da Tesla ironiza: “Dava-me uma grande vantagem”",
+    "description": "O líder checheno levou a sério o combate sugerido por Musk que o opunha a Putin. Ramzan Kadyrov aconselhou o dono do Tesla a \"aumentar os músculos\", chamando-o \"Elona\". Magnata respondeu com ironia.",
+    "url": "https://observador.pt/2022/03/16/antes-do-duelo-com-putin-lider-checheno-aconselha-musk-a-treinar-dono-da-tesla-ironiza-dava-me-uma-grande-vantagem/",
+    "urlToImage": "https://wm.observador.pt/wm/obs/l/https%3A%2F%2Fbordalo.observador.pt%2Fv2%2Frs%3Afill%3A770%3A403%2Fc%3A2000%3A1126%3Anowe%3A0%3A44%2Fq%3A85%2Fplain%2Fhttps%3A%2F%2Fs3.observador.pt%2Fwp-content%2Fuploads%2F2021%2F01%2F07181233%2F30141107.jpg",
+    "publishedAt": "2022-03-16T00:40:29Z",
+    "content": "Tem acesso livre a todos os artigos do Observador por ser nosso assinante.\r\nElon Musk, fundador da SpaceX e presidente executivo da Tesla, desafiou, esta segunda-feira, Vladimir Putin para um duelo. … [+1326 chars]"
+    }
+    ]
+    };
